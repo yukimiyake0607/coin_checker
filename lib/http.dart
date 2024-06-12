ee43cf3c4216bbc13965c7ef54443ac1df5f22dd
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-12 15:46:33 +0900

    add: NetworkHelperクラスを作成
    refs http通信をするため

--- lib/http.dart
+++ lib/http.dart
@@ -1,13 +1,29 @@
-import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 const String apiKey = '8FD458B7-288B-4B9C-85C9-D4A58A90D9BD';
-const String url = 'https://rest.coinapi.io/v1/exchangerate/';
 
 class NetworkHelper {
-  final String virtualCurrency;
-  final String internationalCurrency;
+  final String url;
 
-  NetworkHelper({required this.virtualCurrency, required this.internationalCurrency});
+  NetworkHelper({required this.url});
 
-  
+  Future<dynamic> getData() async {
+    // URLを解析してUriオブジェクトを生成
+    Uri parsedUrl = Uri.parse(url);
+
+    // HTTP GETリクエストを送信
+    final response = await http.get(
+      parsedUrl,
+      headers: {'X-CoinAPI-Key': apiKey},
+    );
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
 }
